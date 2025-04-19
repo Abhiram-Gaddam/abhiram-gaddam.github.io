568349a89d61dc16e6fd9e665a979605a75c5480
saikrishnaj <saikrishna4477@gmail.com>
2025-04-19 17:10:55 +0530

    Updated The Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,6 +8,7 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Loading from './pages/Loading';
+
 import { useState , useEffect } from 'react';
 
 function App() {
@@ -22,22 +24,48 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 flex flex-col">
-      { isLoading? <Loading/> :<Navbar /> } 
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={ isLoading ?  <> </> : <Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path='/loading' element={<Loading/>} />
-          </Routes>
-          <Footer />
-        </main>
+    // <Router>
+    //   <Switch>
+    //   <div className="min-h-screen bg-gray-50 flex flex-col">
+    //   { isLoading? <Loading/> :<Navbar /> } 
+    //     <main className="flex-grow">
+    //       <Routes>
+    //         <Route exact path="/" element={ isLoading ?  <> </> : <Home />} />
+    //         <Route path="/about" element={<About />} />
+    //         <Route path="/projects" element={<Projects />} />
+    //         <Route path="/contact" element={<Contact />} />
+    //         <Route path='/loading' element={<Loading/>} />
+    //         <Route path="*">
+    //       <Redirect to="/" />
+    //     </Route>
+            
+    //       </Routes>
+    //       <Footer />
+    //     </main>
        
-      </div>
-    </Router>
+    //   </div>
+    //   </Switch>
+    // </Router>
+    <Router>
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      {isLoading ? <Loading /> : <Navbar />} 
+      
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={isLoading ? <> </> : <Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/loading" element={<Loading />} />
+
+          {/* Redirect to homepage if route is not found */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+      
+      <Footer />
+    </div>
+  </Router>
   );
 }
 
